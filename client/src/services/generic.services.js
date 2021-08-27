a459e4c20d27dd232417639b4b59a97bdf09aeca
va-ovsyanikov <va.ovsyanikov@gmail.com>
2021-08-27 15:27:05 +0300

    commit

--- client/src/services/generic.services.js
+++ client/src/services/generic.services.js
@@ -1,6 +1,6 @@
 import axios from "axios"
 
-const API_URL = "http://localhost:3001"
+const API_URL = "https://mevn-movie.herokuapp.com/"
 export const request = async ({ url, data, method }) => {
        const response = await axios[method](`${API_URL}/${url}`, data)
        return response
