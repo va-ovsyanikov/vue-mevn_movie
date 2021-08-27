8d62ad8a298c96bc6f09d71b1dce793bff0a22fa
va-ovsyanikov <va.ovsyanikov@gmail.com>
2021-08-27 17:34:18 +0300

    commit

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,18 @@
 module.exports = {
   transpileDependencies: [
     'vuetify'
-  ]
+  ],
+  
+  
+  
+
+    // outputDir: path.resolve(__dirname, '../server/public'),
+    devServer: {
+        proxy: {
+            '/': {
+                target:'https://todo-mevn-app.herokuapp.com/'
+            }
+        }
+    }
+
 }
