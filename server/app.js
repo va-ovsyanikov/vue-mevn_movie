f7abcfaba5671d85085b1f1d9a8a57eeb5ca7855
va-ovsyanikov <va.ovsyanikov@gmail.com>
2021-08-28 21:13:13 +0300

    commit

--- server/app.js
+++ server/app.js
@@ -8,6 +8,8 @@ const PORT = process.env.PORT || 3001
 const routesPost = require('./routes/post')
 const path = require("path")
 
+
+
 app.use("/client/public/",express.static("/public"))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
@@ -16,15 +18,16 @@ app.use(routesPost)
 // app.use(morgan(process.env.LOG_LEVEL))
 
 
-// if(process.env.NODE_ENV === 'production'){
+if(process.env.NODE_ENV === 'production'){
     app.use(express.static(__dirname + '/dist/'))
 
 
-    // app.get('*', function (_, res) {
-    //     const index = path.join(__dirname, 'dist', 'index.html');
-    //     res.sendFile(index);
-    //   });
-// }
+    app.get('/', function (_, res) {
+        const index = path.join(__dirname, 'dist', 'index.html');
+        res.sendFile(index);
+      });
+}
+
 
 
 mongoose.connect("mongodb+srv://Ovsyan:ov240988ov@cluster0.vymbk.mongodb.net/mevn_movie", {
