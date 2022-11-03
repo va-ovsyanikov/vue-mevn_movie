82a2e87b2aebed71d035d1af95478f55419182ba
va-ovsyanikov <va.ovsyanikov@gmail.com>
2022-11-03 16:25:01 +0300

    edit generic.services

--- client/src/services/generic.services.js
+++ client/src/services/generic.services.js
@@ -1,11 +1,11 @@
 import axios from "axios"
 
 
-const baseURL = process.env.NODE_ENV === "production"
-     ? 'http://k36366.fps.by/'
-     : 'http://localhost:3001'
+// const baseURL = process.env.NODE_ENV === "production"
+//      ? 'http://k36366.fps.by/'
+//      : 'http://localhost:3001'
 
-
+const baseURL = 'http://k36366.fps.by/'
 const axiosInstanse = axios.create({
      baseURL,
      proxyHeaders: false,
